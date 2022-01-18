25e8ffdd7c4620119cf8d4a4d1906b6473d81a10
Anier Velasco Sotomayor <anier.velasco@gmail.com>
2022-01-18 13:47:06 +0100

    Add is_admin field to users table.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -56,13 +56,15 @@ class Kernel extends HttpKernel
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'admin' => App\Http\Middleware\AdminStatus::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'honeypot' => \Spatie\Honeypot\ProtectAgainstSpam::class,
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'honeypot' => \Spatie\Honeypot\ProtectAgainstSpam::class
+
     ];
 }
