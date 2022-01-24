40b7c46156db95bcf428f8d49a48306da6919f27
Anier Velasco Sotomayor <anier.velasco@gmail.com>
2022-01-24 02:10:37 +0100

    Implemented methods in UserController.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,20 +3,68 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    
     public function index()
     {
         $users = User::all();
 
         return view('users', ['users' => $users]);
     }
+    
+    public function create()
+    {
+        //
+    }
 
-//    public function show(int $id)
-//    {
-//        $user = User::findOrFail($id);
-//
-//        return view('user', ['user' => $user]);
-//    }
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(int $id)
+    {
+        $user = User::findOrFail($id);
+
+        return view('user_detail', ['user' => $user]);
+    }
+
+    public function edit($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        try{
+            $user = User::findOrFail($id);            
+        }catch(\Exception $e){
+            return redirect()->back()->withErrors(['msg' => 'User not found.']);
+        }
+
+        $fields = $request->toArray();
+        if(isset($fields['_token'])){
+            unset($fields['_token']);
+        }
+        foreach($fields as $key => $val){
+            $user->$key = $val;
+        }
+        $user->save();
+
+        return redirect()->back();
+    }
+
+    public function destroy($id)
+    {
+        try{
+            $user = User::findOrFail($id);
+        }catch(\Exception $e){
+            return redirect()->back()->withErrors(['msg' => 'User not found.']);
+        }
+        $user->delete();
+        return redirect()->back();
+    }
 }
