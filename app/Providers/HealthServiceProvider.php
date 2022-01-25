5775997d57ab1e7132fe609d61d81e300ec4c932
Anier Velasco Sotomayor <anier.velasco@gmail.com>
2022-01-25 10:47:59 +0100

    New checks in the HealthServiceProvider.

--- app/Providers/HealthServiceProvider.php
+++ app/Providers/HealthServiceProvider.php
@@ -2,10 +2,15 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
-use Spatie\Health\Checks\Checks\DatabaseCheck;
-use Spatie\Health\Checks\Checks\UsedDiskSpaceCheck;
 use Spatie\Health\Facades\Health;
+use Illuminate\Support\ServiceProvider;
+use Spatie\Health\Checks\Checks\PingCheck;
+use Spatie\Health\Checks\Checks\CacheCheck;
+use Spatie\Health\Checks\Checks\DatabaseCheck;
+use Spatie\Health\Checks\Checks\ScheduleCheck;
+use Spatie\Health\Checks\Checks\DebugModeCheck;
+use Spatie\Health\Checks\Checks\EnvironmentCheck;
+use Spatie\Health\Checks\Checks\UsedDiskSpaceCheck;
 
 class HealthServiceProvider extends ServiceProvider
 {
@@ -28,6 +33,11 @@ class HealthServiceProvider extends ServiceProvider
         Health::checks([
             UsedDiskSpaceCheck::new(),
             DatabaseCheck::new(),
+            CacheCheck::new(),
+            DebugModeCheck::new(),
+            EnvironmentCheck::new(),
+            PingCheck::new(),
+            ScheduleCheck::new(),
         ]);
     }
 }
