e25126e33f96c6df0a9c7a56da45415568189d2a
Anier Velasco Sotomayor <anier.velasco@gmail.com>
2022-01-27 04:01:44 +0100

    Modified the seeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Message;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +15,21 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \App\Models\User::factory()->create(['is_admin' => true]);
-        \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(30)->create();
+        
+        $users = \App\Models\User::all();
+
+        foreach($users as $user1){
+            if($user1->id < 5)
+            foreach($users as $user2){
+                if($user2->id > 20)
+                if($user1->id <= $user2->id){
+                    Message::factory(1)->create(['sender_id' => $user1->id, 'receiver_id' => $user2->id]);
+                    Message::factory(1)->create(['sender_id' => $user2->id, 'receiver_id' => $user1->id]);
+                }
+            }
+        }
+        
+
     }
 }
