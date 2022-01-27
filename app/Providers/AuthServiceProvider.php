112a4287d7163a4eaa277530d80dba3645ede989
Anier Velasco Sotomayor <anier.velasco@gmail.com>
2022-01-27 03:58:26 +0100

    Add gates for deleting and erasing messages and users.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Message;
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -24,6 +27,19 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('edit-user', function (User $user, User $user2) {
+            return $user->id == $user2->id || $user->is_admin;
+        });
+        Gate::define('delete-user', function (User $user, User $user2) {
+            return $user->id == $user2->id || $user->is_admin;
+        });
+
+        Gate::define('edit-message', function (User $user, Message $message) {
+            return $user->id == $message->sender_id || $user->id == $message->receiver_id;
+        });
+        Gate::define('delete-message', function (User $user, Message $message) {
+            return $user->id == $message->sender_id || $user->id == $message->receiver_id;
+        });
+    
     }
 }
