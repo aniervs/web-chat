b3fb9a57943b5a191c5152294bfcdfc9b5713049
Anier Velasco Sotomayor <anier.velasco@gmail.com>
2022-01-28 03:50:28 +0100

    Change Seeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,22 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory()->create(['is_admin' => true]);
-        \App\Models\User::factory(30)->create();
-
-        $users = \App\Models\User::all();
-
-        foreach ($users as $user1) {
-            if ($user1->id < 5) {
-                foreach ($users as $user2) {
-                    if ($user2->id > 20) {
-                        if ($user1->id <= $user2->id) {
-                            Message::factory(1)->create(['sender_id' => $user1->id, 'receiver_id' => $user2->id]);
-                            Message::factory(1)->create(['sender_id' => $user2->id, 'receiver_id' => $user1->id]);
-                        }
-                    }
-                }
-            }
-        }
+        \App\Models\User::factory()->create(['is_admin' => true, 'name' => 'Anier Velasco', 'email' => 'anier.velasco@gmail.com']);
+        
     }
 }
